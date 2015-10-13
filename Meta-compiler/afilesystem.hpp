38eae2b31e6a1507462da7cd11d077511c8b90b9
Ancel Francois <francoisancel@gmail.com>
2015-10-13 16:50:32 +0200

    update afilesystem to handle more case

--- Meta-compiler/afilesystem.hpp
+++ Meta-compiler/afilesystem.hpp
@@ -6,12 +6,15 @@
 
 namespace astd
 {
-   namespace filesystem
-   {
-      typedef std::experimental::filesystem::path path;
-      using std::experimental::filesystem::exists;
-      using std::experimental::filesystem::remove;
-   }
+namespace filesystem
+{
+   typedef std::experimental::filesystem::path path;
+   typedef std::experimental::filesystem::directory_iterator directory_iterator;
+   using std::experimental::filesystem::exists;
+   using std::experimental::filesystem::remove;
+   using std::experimental::filesystem::is_regular_file;
+   using std::experimental::filesystem::is_directory;
+}
 }
 
 #else
@@ -21,14 +24,17 @@ namespace astd
 
 namespace astd
 {
-   namespace filesystem
-   {
-      typedef boost::filesystem::path path;
-      using boost::filesystem::exists;
-      using boost::filesystem::remove;
-   }
+namespace filesystem
+{
+   typedef boost::filesystem::path path;
+   typedef boost::filesystem::directory_iterator directory_iterator;
+   using boost::filesystem::exists;
+   using boost::filesystem::remove;
+   using boost::filesystem::is_regular_file;
+   using boost::filesystem::is_directory;
+}
 }
 
 #endif
 
-#endif //!AFILESYSTEM_HPP
+#endif //! AFILESYSTEM_HPP
