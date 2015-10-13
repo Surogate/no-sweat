f2f3730ba231d8b1ead8def2d1744315b5bdfadb
Ancel Francois <francoisancel@gmail.com>
2015-10-13 11:37:27 +0200

    Moving to linux standard input

--- Meta-compiler/input.hpp
+++ Meta-compiler/input.hpp
@@ -3,12 +3,12 @@
 
 #include <iostream>
 
-#include "boost\program_options\variables_map.hpp"
-#include "boost\program_options\options_description.hpp"
-#include "boost\program_options\positional_options.hpp"
-#include "boost\program_options\options_description.hpp"
-#include "boost\program_options\cmdline.hpp"
-#include "boost\program_options\parsers.hpp"
+#include "boost/program_options/variables_map.hpp"
+#include "boost/program_options/options_description.hpp"
+#include "boost/program_options/positional_options.hpp"
+#include "boost/program_options/options_description.hpp"
+#include "boost/program_options/cmdline.hpp"
+#include "boost/program_options/parsers.hpp"
 
 #include "trim.hpp"
 #include "error_status.hpp"
